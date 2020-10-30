04e7f0dc5cdad478be26b287c4bdd67caec24bba
francisudeji <francis.udejiofficial@gmail.com>
2020-10-30 05:30:44 +0100

    Display table and adjust design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,85 @@
 import Head from 'next/head'
-import { useQuery } from 'react-query'
-import { fetchFilms } from '@/utils/client'
+import { useQuery, useMutation } from 'react-query'
+import { fetchFilms, fetchFilm } from '@/utils/queries'
 import { Dropdown } from '@/components/dropdown'
-import { ReactQueryConfigProvider } from 'react-query'
-import { parseISO, isAfter } from 'date-fns'
+import { parseISO, isBefore } from 'date-fns'
+import { QueryCache } from 'react-query'
+import { dehydrate } from 'react-query/hydration'
+import { TableLayout } from '@/components/table'
 
-export default function Home() {
-  const { status, data, error } = useQuery('films', fetchFilms)
+export default function Home({ films }) {
+  const [mutate, { status, data: characters, error }] = useMutation(fetchFilm)
 
-  if (status === 'loading') {
-    return <p>Loading...</p>
-  }
+  // const data = characters.map(({ data }) => ({
+  //   name: data.name,
+  //   gender: data.gender,
+  //   height: data.height
+  // }))
 
-  if (error) {
-    return <p>Error: {error}</p>
-  }
+  const columns = [
+    {
+      Header: 'Name',
+      accessor: 'name',
+      sortType: 'basic'
+    },
+    {
+      Header: 'Gender',
+      accessor: 'gender',
+      sortType: 'basic'
+    },
+    {
+      Header: 'Height',
+      accessor: 'height',
+      sortType: 'basic'
+    }
+  ]
 
   function sorter(a, b) {
-    return isAfter(parseISO(a), parseISO(b))
+    return isBefore(parseISO(a), parseISO(b))
   }
 
   return (
-    <ReactQueryConfigProvider config={{ queries: { refetchOnWindowFocus: false } }}>
-      <div className="container mx-auto p-3">
-        <Head>
-          <title>Star Wars App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div className="container mx-auto p-3">
+      <Head>
+        <title>Star Wars App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <div className="-mt-32">
-          <h1 className="text-sw-yellow mx-auto block text-center text-5xl font-starjedi mb-6">
-            Star Wars Movies
-          </h1>
-          <Dropdown
-            items={data.sort(sorter)}
-            onChange={(selection) => {
-              console.log(selection)
-            }}
-          />
-        </div>
+      <div className="-mt-32">
+        <h1 className="text-sw-yellow mx-auto block text-center text-5xl font-starjedi mb-6">
+          Star Wars Movies
+        </h1>
+        <Dropdown
+          items={films.sort(sorter)}
+          onChange={(selection) => {
+            if (selection) mutate(selection.characters)
+          }}
+        />
       </div>
-    </ReactQueryConfigProvider>
+      {characters !== undefined ? (
+        <TableLayout
+          columns={columns}
+          data={characters.map(({ data }) => ({
+            name: data.name,
+            gender: data.gender,
+            height: data.height
+          }))}
+        />
+      ) : (
+        <span>Loading...</span>
+      )}
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const queryCache = new QueryCache()
+
+  await queryCache.prefetchQuery('films', fetchFilms)
+
+  return {
+    props: {
+      films: dehydrate(queryCache).queries[0].data
+    }
+  }
 }

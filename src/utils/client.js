6149e4a537e97fd004de8409bdef02cf7261b6d1
francisudeji <francis.udejiofficial@gmail.com>
2020-10-30 05:31:59 +0100

    Clean up

--- src/utils/client.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
-
-export async function fetchFilms() {
-  return await axios.get(`${apiUrl}/films`).then((res) => res.data.results)
-}

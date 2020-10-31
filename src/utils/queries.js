0979581561fa20c26b897931d881d8cb469b4b0e
francisudeji <francis.udejiofficial@gmail.com>
2020-10-31 15:35:43 +0100

    Hardcode API URL

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
-const proxyUrl = process.env.NEXT_PUBLIC_PROXY_URL
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL
+// const proxyUrl = process.env.NEXT_PUBLIC_PROXY_URL
 
 export async function fetchFilms() {
-  return await axios.get(`${apiUrl}/films`).then((res) => res.data.results)
+  return await axios.get(`https://swapi.dev/api/films`).then((res) => res.data.results)
 }
 
 export async function fetchFilm(characters) {
   return await axios
     .all(
       characters.map((characterUrl) =>
-        axios.get(`${proxyUrl}${String(characterUrl).replace('http', 'https')}`)
+        axios.get(`${String(characterUrl).replace('http', 'https')}`)
       )
     )
     .then((res) => res)
